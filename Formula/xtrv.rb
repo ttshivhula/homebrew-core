5548fd328fd05b5fc706eae2e6d9c37af08791e6
Tshivhula Tshilidzi <ttshivhula@users.noreply.github.com>
2020-03-29 04:56:06 +0200

    Create xtrv.rb

---
+++ Formula/xtrv.rb
@@ -0,0 +1,16 @@
+class Xtrv < Formula
+  desc "A simple archiving utility"
+  homepage ""
+  url "https://github.com/ttshivhula/xtrv/archive/v0.0.3.tar.gz"
+  sha256 "f92274fbd719ffd1e43cfe1a1b37fd81d0692fb67cb37b7f131d64cca738a9a1"
+  head "https://github.com/ttshivhula/xtrv.git"
+
+  def install
+    system "make"
+    system "mkdir #{prefix}/bin"
+    system "mkdir #{prefix}/share"
+    system "cp xtrv #{prefix}/bin/"
+    system "cp unxtrv #{prefix}/bin/"
+    system "cp -rf man #{prefix}/share"
+  end
+end
